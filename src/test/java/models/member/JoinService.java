05948be1798e2e7c6f4ae06cb302bd5d41adad0d
xlfkshwkd <xlfkshwkd@naver.com>
2023-10-20 13:59:40 +0900

    의존성 추가 및 회원가입

---
+++ src/test/java/models/member/JoinService.java
@@ -0,0 +1,23 @@
+package models.member;
+
+import commons.BadRequestException;
+import commons.Validator;
+
+public class JoinService {
+    private Validator validator;
+    private MemberDao memberDao;
+
+    public JoinService(Validator validator, MemberDao memberDao) {
+        this.validator = validator;
+        this.memberDao = memberDao;
+    }
+
+    public void join(Member member) {
+        validator.check(member);
+
+        String userId = member.getUserId();
+        if (userId == null || userId.isBlank()) {
+            throw new BadRequestException("아이디를 입력하세요.");
+        }
+    }
+}
---
+++ src/test/java/tests/JoinServiceTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import commons.BadRequestException;
+import models.member.JoinService;
+import models.member.JoinValidator;
+import models.member.Member;
+import models.member.MemberDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("회원가입 기능 단위테스트")
+public class JoinServiceTest {
+    private JoinService joinService;
+
+    @BeforeEach
+    void init() {
+      //  joinService = new JoinService();
+        joinService = new JoinService(new JoinValidator(), new MemberDao());
+    }
+
+    private Member getMember() {
+        String userPw = "12345678";
+        return Member.builder()
+                .userId("user" + System.currentTimeMillis())
+                .userPw(userPw)
+                .confirmUserPw(userPw)
+                .userNm("사용자")
+                .email("user@test.org")
+                .agree(true)
+                .build();
+    }
+    @Test
+    @DisplayName("회원가입 성공시 예외발생하지 않음")
+    void joinSuccess() {
+        assertDoesNotThrow(() -> {
+            joinService.join(getMember());
+        });
+    }
+    @Test
+    @DisplayName("필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명, 이메일, 회원가입약관 동의), 검증 실패시 BadRequestException 발생")
+    void requiredFieldCheck() {
+        // 아이디(userId)가 null 또는 빈값("")
+        assertThrows(BadRequestException.class, () -> {
+            Member member = getMember();
+            member.setUserId(null);
+            joinService.join(member);
+            member.setUserId("   ");
+            joinService.join(member);
+        });
+    }
+}

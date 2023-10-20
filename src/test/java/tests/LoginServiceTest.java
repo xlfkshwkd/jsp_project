332c3f1fb3e0b332af370b52e51754ee2dcc9177
xlfkshwkd <xlfkshwkd@naver.com>
2023-10-20 17:11:53 +0900

    로그인 예시 추가

--- src/test/java/tests/LoginServiceTest.java
+++ src/test/java/tests/LoginServiceTest.java
@@ -3,23 +3,38 @@ package tests;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import models.member.JoinService;
 import models.member.LoginService;
+import models.member.Member;
+import models.member.ServiceManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("로그인 기능 단위 테스트")
+@ExtendWith(MockitoExtension.class)
+
 public class LoginServiceTest {
 
     private LoginService loginService;
+    @Mock
     private HttpServletRequest request;
 
+    private Member member;
 
     @BeforeEach
     void init(){
         loginService =new LoginService();
+        member =getMember();
+
+        JoinService joinService = ServiceManager.getInstance().joinService();;
+        joinService.join(member);
+
     }
     @Test
     @DisplayName("로그인 성공시 예외발생 안함")
@@ -30,5 +45,17 @@ public class LoginServiceTest {
     }
 
     //아아ㅏㅏ아ㅏㅏㅏㅏㅏ
+
+    private Member getMember() {
+        String userPw = "12345678";
+        return Member.builder()
+                .userId("user" + System.currentTimeMillis())
+                .userPw(userPw)
+                .confirmUserPw(userPw)
+                .userNm("사용자")
+                .email("user@test.org")
+                .agree(true)
+                .build();
+    }
 
 }

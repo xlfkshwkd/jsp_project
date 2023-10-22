fbb4fb422e6cf2e2b1e13710c81dd84d63c891e5
xlfkshwkd <xlfkshwkd@naver.com>
2023-10-22 17:06:20 +0900

    비번 해시

--- src/test/java/models/member/MemberDao.java
+++ src/test/java/models/member/MemberDao.java
@@ -8,22 +8,21 @@ import java.util.Map;
 public class MemberDao {
     private static Map<String, Member> members = new HashMap<>();
 
-    public void register(Member member) {
-
-        String userPw = BCrypt.hashpw(member.getUserPw(), BCrypt.gensalt(12));
+    public void register(Member member){
+        String userPw = BCrypt.hashpw(member.getUserPw(), BCrypt.gensalt(12)); //유동 해시 BCrypt로 암호화
         member.setUserPw(userPw);
-        members.put(member.getUserId(), member);
+        members.put(member.getUserId(),member);
     }
 
-    public Member get(String userId) {
+    public Member get(String userId){
         return members.get(userId);
     }
 
-    public boolean exists(String userId) {
+    public boolean exists(String userId){
         return members.containsKey(userId);
     }
 
-    public static void clearData() {
+    public static void clearData(){ //DB 연동시 필요 없음.
         members.clear();
     }
 }
